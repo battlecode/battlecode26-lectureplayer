272c319dd35198c514f3deda3cdc873c95f8b20d
compiler64 <maxmisterka64@gmail.com>
2026-01-07 19:10:33 -0500

    update

--- src/lectureplayer/RobotPlayer.java
+++ src/lectureplayer/RobotPlayer.java
@@ -18,35 +18,18 @@ public class RobotPlayer {
     public static State currentState = State.INITIALIZE;
 
     public static int numRatsSpawned = 0;
+    public static int turnsSinceCarry = 0;
 
     public static Direction[] directions = Direction.values();
 
     public static void run(RobotController rc) {
         while (true) {
             try {
-                MapLocation myLoc = rc.getLocation();
-
                 if (rc.getType().isRatKingType()) {
-                    int currentCost = rc.getCurrentRatCost();
-
-                    if (currentCost <= 10 || rc.getAllCheese() > currentCost + 2500) {
-                        MapLocation[] potentialSpawnLocations = rc.getAllLocationsWithinRadiusSquared(rc.getLocation(), 8);
-                        
-                        for (MapLocation loc : potentialSpawnLocations) {
-                            if (rc.canBuildRat(loc)) {
-                                rc.buildRat(loc);
-                                numRatsSpawned++;
-                                break;
-                            }
-                        }
-                    }
-
-                    moveRandom(rc);
-
-                    // TODO make more efficient and expand communication in the communication lecture
-                    rc.writeSharedArray(0, myLoc.x);
-                    rc.writeSharedArray(1, myLoc.y);
+                    runRatKing(rc);
                 } else {
+                    turnsSinceCarry++;
+
                     switch (currentState) {
                         case INITIALIZE:
                             if (rc.getRoundNum() < 30 || rc.getCurrentRatCost() <= 10) {
@@ -57,113 +40,16 @@ public class RobotPlayer {
 
                             break;
                         case FIND_CHEESE:
-                            // search for cheese
-                            MapInfo[] nearbyInfos = rc.senseNearbyMapInfos();
-
-                            for (MapInfo info : nearbyInfos) {
-                                if (info.getCheeseAmount() > 0) {
-                                    Direction toCheese = myLoc.directionTo(info.getMapLocation());
-
-                                    if (rc.canTurn()) {
-                                        rc.turn(toCheese);
-                                        break;
-                                    }
-                                }
-                            }
-
-                            for (Direction dir : directions) {
-                                MapLocation loc = myLoc.add(dir);
-                                
-                                if (rc.canPickUpCheese(loc)) {
-                                    rc.pickUpCheese(loc);
-
-                                    if (rc.getRawCheese() >= 10) {
-                                        currentState = State.RETURN_TO_KING;
-                                    }
-                                }
-                            }
-
-                            moveRandom(rc);
+                            runFindCheese(rc);
                             break;
                         case RETURN_TO_KING:
-                            MapLocation kingLoc = new MapLocation(rc.readSharedArray(0), rc.readSharedArray(1));
-                            Direction toKing = myLoc.directionTo(kingLoc);
-                            MapLocation nextLoc = myLoc.add(toKing);
-
-                            if (rc.canTurn()) {
-                                rc.turn(toKing);
-                            }
-
-                            if (rc.canRemoveDirt(nextLoc)) {
-                                rc.removeDirt(nextLoc);
-                            }
-
-                            // TODO replace with pathfinding for the pathfinding lecture
-                            if (rc.canMove(toKing)) {
-                                rc.move(toKing);
-                            }
-
-                            int rawCheese = rc.getRawCheese();
-
-                            if (rawCheese == 0) {
-                                currentState = State.FIND_CHEESE;
-                                break;
-                            }
-                            
-                            if (rc.canSenseLocation(kingLoc)) {
-                                RobotInfo[] kingLocations = rc.senseNearbyRobots(kingLoc, 8, rc.getTeam());
-
-                                for (RobotInfo robotInfo : kingLocations) {
-                                    if (robotInfo.getType().isRatKingType()) {
-                                        MapLocation actualKingLoc = robotInfo.getLocation();
-
-                                        if (rc.canTransferCheese(actualKingLoc, rawCheese)) {
-                                            System.out.println("Transferred " + rawCheese + " cheese to king at " + kingLoc + ": I'm at " + myLoc);
-                                            rc.transferCheese(actualKingLoc, rawCheese);
-                                            currentState = State.FIND_CHEESE;
-                                        }
-
-                                        break;
-                                    }
-                                }
-                            }
-
+                            runReturnToKing(rc);
                             break;
                         case BUILD_TRAPS:
-                            for (Direction dir : directions) {
-                                MapLocation loc = myLoc.add(dir);
-                                boolean catTraps = rand.nextBoolean();
-                                
-                                if (catTraps && rc.canPlaceCatTrap(loc)) {
-                                    System.out.println("Built cat trap at " + loc);
-                                    rc.placeCatTrap(loc);
-                                } else if (rc.canPlaceRatTrap(loc)) {
-                                    System.out.println("Built rat trap at " + loc);
-                                    rc.placeRatTrap(loc);
-                                }
-                            }
-
-                            if (rand.nextDouble() < 0.1) {
-                                currentState = State.EXPLORE_AND_ATTACK;
-                            }
-
-                            moveRandom(rc);
+                            runBuildTraps(rc);
                             break;
                         case EXPLORE_AND_ATTACK:
-                            moveRandom(rc);
-
-                            for (Direction dir : directions) {
-                                MapLocation loc = myLoc.add(dir);
-
-                                if (rc.canAttack(loc)) {
-                                    rc.attack(loc);
-                                }
-                            }
-
-                            if (rand.nextDouble() < 0.1) {
-                                currentState = State.BUILD_TRAPS;
-                            }
-
+                            runExploreAndAttack(rc);
                             break;
                     }
                 }
@@ -194,6 +80,146 @@ public class RobotPlayer {
             if (rc.canTurn()) {
                 rc.turn(random);
             }
+        }
+    }
+
+    public static void runRatKing(RobotController rc) throws GameActionException {
+        int currentCost = rc.getCurrentRatCost();
+
+        if (currentCost <= 10 || rc.getAllCheese() > currentCost + 2500) {
+            MapLocation[] potentialSpawnLocations = rc.getAllLocationsWithinRadiusSquared(rc.getLocation(), 8);
+            
+            for (MapLocation loc : potentialSpawnLocations) {
+                if (rc.canBuildRat(loc)) {
+                    rc.buildRat(loc);
+                    numRatsSpawned++;
+                    break;
+                }
+            }
+        }
+
+        moveRandom(rc);
+
+        // TODO make more efficient and expand communication in the communication lecture
+        rc.writeSharedArray(0, rc.getLocation().x);
+        rc.writeSharedArray(1, rc.getLocation().y);
+    }
+
+    public static void runFindCheese(RobotController rc) throws GameActionException {
+        // search for cheese
+        MapInfo[] nearbyInfos = rc.senseNearbyMapInfos();
+
+        for (MapInfo info : nearbyInfos) {
+            if (info.getCheeseAmount() > 0) {
+                Direction toCheese = rc.getLocation().directionTo(info.getMapLocation());
+
+                if (rc.canTurn()) {
+                    rc.turn(toCheese);
+                    break;
+                }
+            }
+        }
+
+        for (Direction dir : directions) {
+            MapLocation loc = rc.getLocation().add(dir);
+            
+            if (rc.canPickUpCheese(loc)) {
+                rc.pickUpCheese(loc);
+
+                if (rc.getRawCheese() >= 10) {
+                    currentState = State.RETURN_TO_KING;
+                }
+            }
+        }
+
+        moveRandom(rc);
+    }
+
+    public static void runReturnToKing(RobotController rc) throws GameActionException {
+        MapLocation kingLoc = new MapLocation(rc.readSharedArray(0), rc.readSharedArray(1));
+        Direction toKing = rc.getLocation().directionTo(kingLoc);
+        MapLocation nextLoc = rc.getLocation().add(toKing);
+
+        if (rc.canTurn()) {
+            rc.turn(toKing);
+        }
+
+        if (rc.canRemoveDirt(nextLoc)) {
+            rc.removeDirt(nextLoc);
+        }
+
+        // TODO replace with pathfinding for the pathfinding lecture
+        if (rc.canMove(toKing)) {
+            rc.move(toKing);
+        }
+
+        int rawCheese = rc.getRawCheese();
+
+        if (rawCheese == 0) {
+            currentState = State.FIND_CHEESE;
+        }
+        
+        if (rc.canSenseLocation(kingLoc)) {
+            RobotInfo[] kingLocations = rc.senseNearbyRobots(kingLoc, 8, rc.getTeam());
+
+            for (RobotInfo robotInfo : kingLocations) {
+                if (robotInfo.getType().isRatKingType()) {
+                    MapLocation actualKingLoc = robotInfo.getLocation();
+
+                    if (rc.canTransferCheese(actualKingLoc, rawCheese)) {
+                        System.out.println("Transferred " + rawCheese + " cheese to king at " + kingLoc + ": I'm at " + rc.getLocation());
+                        rc.transferCheese(actualKingLoc, rawCheese);
+                        currentState = State.FIND_CHEESE;
+                    }
+
+                    break;
+                }
+            }
+        }
+    }
+
+    public static void runBuildTraps(RobotController rc) throws GameActionException {
+        for (Direction dir : directions) {
+            MapLocation loc = rc.getLocation().add(dir);
+            boolean catTraps = rand.nextBoolean();
+            
+            if (catTraps && rc.canPlaceCatTrap(loc)) {
+                System.out.println("Built cat trap at " + loc);
+                rc.placeCatTrap(loc);
+            } else if (rc.canPlaceRatTrap(loc)) {
+                System.out.println("Built rat trap at " + loc);
+                rc.placeRatTrap(loc);
+            }
+        }
+
+        if (rand.nextDouble() < 0.1) {
+            currentState = State.EXPLORE_AND_ATTACK;
+        }
+
+        moveRandom(rc);
+    }
+
+    public static void runExploreAndAttack(RobotController rc) throws GameActionException {
+        moveRandom(rc);
+
+        if (rc.canThrowRat() && turnsSinceCarry >= 3) {
+            rc.throwRat();
+        }
+
+        for (Direction dir : directions) {
+            MapLocation loc = rc.getLocation().add(dir);
+
+            if (rc.canCarryRat(loc)) {
+                rc.carryRat(loc);
+            }
+
+            if (rc.canAttack(loc)) {
+                rc.attack(loc);
+            }
+        }
+
+        if (rand.nextDouble() < 0.1) {
+            currentState = State.BUILD_TRAPS;
         }
     }
 }
